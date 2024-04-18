f7b6ef1beddc3727b81c0c9acc18402fed558e10
Kitwana Akil <kakil@me.com>
2024-04-18 14:29:45 -0400

    Setup the web gui.

--- web.py
+++ web.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    print(todo)
+
 todos = functions.get_todos()
 
 st.title("My Todo App")
@@ -8,4 +14,6 @@ st.title("My Todo App")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo", placeholder="Add new todo...")
+st.text_input(label="Enter a todo", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
+
+st.session_state
